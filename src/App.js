fef9b3aabdf0450d39b3e5a4951c25d872337ffe
AliceUwase <a.uwase3@alustudent.com>
2025-09-29 15:40:57 +0200

    update component in App routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './Components/VendorApp/LogIn';
 import { SignUp } from './Components/VendorApp/SignUp'; 
-import { SelectionCard } from './Components/VendorApp/SelectionCard';
 import { Landing } from './Components/VendorApp/Landing';
 
 
@@ -10,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SelectionCard />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/landing" element={<Landing />} />
